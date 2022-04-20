97571ee9e6992e506388308a17159fcc799f0570
CCR-ice <64931368+CCR-ice@users.noreply.github.com>
2022-04-20 15:53:06 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,19 @@
 echo "Updating..." &&
 apt update && apt upgrade || dpkg --configure -a &&
+echo "Done." &&
 echo "Fetching package..." &&
 curl https://launcher.mojang.com/download/Minecraft.deb -o pkg.deb &&
 echo "Done." &&
-echo "Installing package..." &&
+echo "Installing package 1..." &&
 dpkg -i pkg.deb &&
 apt -f install &&
 echo "Done." &&
+echo "Updating packages..." &&
+sudo apt-get update -y &&
+echo "Done." &&
+echo "Installing package 2..." &&
+sudo apt-get install -y libsecret-1-0 &&
+echo "Done" &&
 echo "Clening up..." &&
 rm -rf pkg.deb &&
 echo "Done."
