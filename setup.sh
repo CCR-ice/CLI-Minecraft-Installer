24e381ecdbe4a1109d9b5ffb4cd81168f627b3f6
CCR-ice <64931368+CCR-ice@users.noreply.github.com>
2022-04-23 14:33:04 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,24 @@
-echo "Updating..." &&
-apt update && apt upgrade || dpkg --configure -a &&
-echo "Done." &&
-echo "Fetching package..." &&
-curl https://launcher.mojang.com/download/Minecraft.deb -o pkg.deb &&
-echo "Done." &&
-echo "Installing package 1..." &&
-dpkg -i pkg.deb ||
-apt --fix-broken install &&
-apt -f install &&
-echo "Done." &&
-echo "Updating packages..." &&
-apt-get update -y &&
-echo "Done." &&
-echo "Installing package 2..." &&
-apt-get install -y libsecret-1-0 &&
-echo "Done" &&
-echo "Clening up..." &&
-rm -rf pkg.deb &&
-echo "Done." &&
-echo "Starting minecraft-launcher" &&
-minecraft-launcher &&
-echo "Done."
+}
+  echo "Updating..." &&
+  apt update && apt upgrade || dpkg --configure -a &&
+  echo "Done." &&
+  echo "Fetching package..." &&
+  curl https://launcher.mojang.com/download/Minecraft.deb -o pkg.deb &&
+  echo "Done." &&
+  echo "Installing package 1..." &&
+  dpkg -i pkg.deb ||
+  apt --fix-broken install &&
+  apt -f install &&
+  echo "Done." &&
+  echo "Updating packages..." &&
+  apt-get update -y &&
+  echo "Done." &&
+  echo "Installing package 2..." &&
+  apt-get install -y libsecret-1-0 &&
+  echo "Done" &&
+  echo "Clening up..." &&
+  rm -rf pkg.deb &&
+  echo "Done." &&
+  minecraft-launcher;
+} ||
+echo "The installation was not fully secusessful."
